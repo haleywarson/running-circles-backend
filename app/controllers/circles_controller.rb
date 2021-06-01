7d1817e45d1fc06e03ed27ce2fb9bdf5a4f0e795
Haley Warson <haleywarson@gmail.com>
2021-06-01 12:34:35 -0600

    Updates controllers

--- app/controllers/circles_controller.rb
+++ app/controllers/circles_controller.rb
@@ -1,2 +1,34 @@
 class CirclesController < ApplicationController
+    
+    before_action :find_circle, only: [:show, :update, :destroy]
+
+    def index
+        # get
+        @circles = Circle.all
+        render json: @circles
+    end
+    
+    def show
+        render json: @circle
+    end
+    
+    def create
+        @circle = Circle.create name: params[:name]
+        render json: @circle
+    end
+    
+    def update
+        @circle.update name: params[:name]
+        render json: @circle
+    end
+    
+    def destroy
+        @circle.destroy
+    End
+    private
+    
+    def find_circle
+        @circle = Circle.find params[:id]
+    End
+
 end
--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,2 +1,38 @@
 class RunsController < ApplicationController
+
+    before_action :find_run, only: [:show, :update, :destroy]
+
+    def index
+        @runs = Run.all
+        render json: @runs
+    end
+    
+    def show
+        render json: @run
+    end
+    
+    def create
+        @run = Run.create!(runs_params)
+        render json: @run
+    end
+    
+    def update
+        @run.update(runs_params)
+        render json: @run
+    end
+    
+    def destroy
+        @run.destroy
+    end
+
+    private
+    
+    def find_run
+        @run = Run.find params[:id]
+    end
+
+    def runs_params
+        params.require(:run).permit(:name, :age, :date, :location, :time)
+    end 
+
 end
