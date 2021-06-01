b327fa987080830b6315e46545d3606e84e8592e
Haley Warson <haleywarson@gmail.com>
2021-06-01 12:26:48 -0600

    Seeds database, updates associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+
     has_many :user_circles
-    has_many :circles, through: user_circles
+    has_many :circles, through: :user_circles
+
     has_many :user_runs
     has_many :runs, through: :user_runs
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,102 +4,102 @@ User.destroy_all
 Circle.destroy_all 
 Run.destroy_all 
 
-haley = User.create(
+haley = User.create!(
     username: "haley",
     password: "haley123"
 )
 
-tim = User.create(
+tim = User.create!(
     username: "tim",
     password: "tim123"
 )
 
-hattie = User.create(
+hattie = User.create!(
     username: "hattie",
     password: "hattie123"
 )
 
-colette = User.create(
+colette = User.create!(
     username: "colette",
     password: "colette123"
 )
 
-parents = Circle.create(name: "parents")
-kids = Circle.create(name: "kids")
-girls = Circle.create(name: "girls")
+parents = Circle.create!(name: "parents")
+kids = Circle.create!(name: "kids")
+girls = Circle.create!(name: "girls")
 
-park_run = Run.create(
+park_run = Run.create!(
     name: "Park run",
     location: "Central park",
     date: "05-30-2021",
     time: "4:00 PM"
 )
 
-neighborhood_run = Run.create(
+neighborhood_run = Run.create!(
     name: "Neighborhood run",
     location: "Westerly creek",
     date: "05-30-2021",
     time: "4:00 PM"
 )
 
-track_run = Run.create(
+track_run = Run.create!(
     name: "Track run",
     location: "Track",
     date: "05-30-2021",
     time: "4:00 PM"
 )
 
-UserCircle.create(
+UserCircle.create!(
     user_id: haley.id,
     circle_id: girls.id 
 )
 
-UserCircle.create(
+UserCircle.create!(
     user_id: haley.id,
     circle_id: parents.id 
 )
 
-UserCircle.create(
+UserCircle.create!(
     user_id: tim.id,
     circle_id: parents.id 
 )
 
-UserCircle.create(
+UserCircle.create!(
     user_id: hattie.id,
     circle_id: kids.id 
 )
 
-UserCircle.create(
+UserCircle.create!(
     user_id: hattie.id,
     circle_id: girls.id 
 )
 
-UserCircle.create(
+UserCircle.create!(
     user_id: colette.id,
     circle_id: kids.id 
 )
 
-UserRun.create(
+UserRun.create!(
     user_id: haley.id,
     run_id: park_run.id
 )
 
-UserRun.create(
+UserRun.create!(
     user_id: tim.id,
     run_id: park_run.id
 )
 
-UserRun.create(
+UserRun.create!(
     user_id: hattie.id,
     run_id: park_run.id
 )
 
-UserRun.create(
+UserRun.create!(
     user_id: haley.id,
     run_id: neighborhood_run.id
 )
 
-UserRun.create(
+UserRun.create!(
     user_id: haley.id,
     run_id: track_run.id
 )
