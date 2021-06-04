93b8ffc5b29c339196dea4339c6e292ba2b6686b
Haley Warson <haleywarson@gmail.com>
2021-06-04 13:09:22 -0600

    Adds POST route to user_runs controller.

--- app/controllers/user_runs_controller.rb
+++ app/controllers/user_runs_controller.rb
@@ -1,2 +1,20 @@
 class UserRunsController < ApplicationController
+
+    def index
+        @user_runs = UserRun.all 
+        render json: @user_runs
+    end 
+        
+    def create
+        @user_run = UserRun.create(user_run_params)
+
+        render json: @user_run, status: :created 
+    end 
+
+    private
+
+    def user_run_params
+        params.require(:user_run).permit(:user_id, :run_id)
+    end 
+
 end
